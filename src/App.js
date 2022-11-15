557cbf3b907ba9a2f2ca56e8db2d808ed5dbbc65
Adanpradana <adanizkahariz@gmail.com>
2022-11-15 15:23:31 +0700

    login page done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./components/empty-todo/emptyTodo.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landing-page/landing-page";
 import Todolist from "./Todolist";
+import Login from "./components/landing-page/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="login" element={<Login />} />
+
       {/* <Route path="/todolist" element={<Todolist />} /> */}
     </Routes>
   );
