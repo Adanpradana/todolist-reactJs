1bdef22c322d09676e083c61b22322f6fa80b70c
Adanpradana <adanizkahariz@gmail.com>
2023-01-16 14:04:17 +0700

    refactoring folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import { Routes, Route, Outlet, useNavigate } from "react-router-dom";
-import LandingPage from "./components/landing-page/landing-page";
-import Register from "./components/Register";
-import Todolist from "./Todolist";
-import Login from "./components/landing-page/Login";
+import LandingPage from "./components/landing-page/";
+import Register from "./components/register";
+import Todolist from "./todolist";
+import Login from "./components/login";
 import "./components/index.css";
 import "./components/empty-todo/emptyTodo.css";
 
 function App() {
-  // useEffect(() => {
-  //   axios({
-  //     method: "GET",
-  //     url: `https://type.fit/api/quotes`,
-  //   })
-  //     .then((data) => {
-  //       const rand = Math.floor(Math.random() * data.data.length);
-  //       setQuotes(data.data[rand]);
-  //     })
-  //     .catch((err) => console.log(err))
-  //     .finally(() => setLoading(false));
-  // }, []);
-
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
