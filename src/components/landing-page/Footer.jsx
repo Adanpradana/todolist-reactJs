a9c21daa6380492ec3ddf48ed29b3f23e36657f3
Adanpradana <adanizkahariz@gmail.com>
2023-01-17 23:10:25 +0700

    hero section done

---
+++ src/components/landing-page/Footer.jsx
@@ -0,0 +1,72 @@
+import whatsapp from "./../../assets/sosial-media/whatsapp-white.svg";
+import instagram from "./../../assets/sosial-media/instagram.svg";
+import linkedin from "./../../assets/sosial-media/linkedin.svg";
+import email from "./../../assets/sosial-media/gmail_old.svg";
+import github from "./../../assets/sosial-media/github.svg";
+
+const Footer = () => {
+  return (
+    <footer className=" items-center mx-auto">
+      <div className="text-center h-full">
+        <p className="text-md">We'd love to hear from you !</p>
+        <div className=" w-full flex justify-center gap-2 py-2">
+          <div className="cursor-pointer">
+            <img
+              onClick={() =>
+                window.open("https://www.instagram.com/Adanpradanaa/")
+              }
+              src={instagram}
+              alt=""
+              className="w-7 h-7 bg-slate-400 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-1 rounded-md"
+            />
+          </div>
+          <div className="cursor-pointer">
+            <img
+              onClick={() =>
+                window.open("https://api.whatsapp.com/send?phone=081999984880")
+              }
+              src={whatsapp}
+              alt=""
+              className="w-7 h-7 bg-slate-400 hover:bg-[#25D366] p-1 rounded-md"
+            />
+          </div>
+          <div className="cursor-pointer">
+            <img
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/adan-pradana-31a7041a1/"
+                )
+              }
+              src={linkedin}
+              alt=""
+              className="w-7 h-7 bg-slate-400 hover:bg-[#0077b5] p-1 rounded-md"
+            />
+          </div>
+          <div className="cursor-pointer">
+            <img
+              onClick={() => window.open("mailto:adanrizkahariz@gmail.com")}
+              src={email}
+              alt=""
+              className="w-7 h-7 bg-slate-400 hover:bg-white p-1 rounded-md"
+            />
+          </div>
+          <div className="cursor-pointer">
+            <img
+              onClick={() => window.open("https://github.com/Adanpradana")}
+              src={github}
+              alt=""
+              className="w-7 h-7 bg-slate-400 hover:bg-[#171515] p-1 rounded-md"
+            />
+          </div>
+        </div>
+        <div className="py-4">
+          <p className="text-xs text-slate-400">
+            &#169; Copyright <span className="font-semibold">Adan Pradana</span>{" "}
+            2022-2023.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -1,13 +1,24 @@
 import Typewriter from "typewriter-effect";
 import heroImg from "../../assets/38.png";
 import { Link } from "react-router-dom";
+import CountUp from "react-countup";
+import { useState } from "react";
+import { useEffect } from "react";
 const Hero = () => {
+  const [randomUsers, setRandomUsers] = useState(0);
+
+  useEffect(() => {
+    const number = 60;
+    const random = Math.floor(Math.random() * number);
+    setRandomUsers(random);
+  }, []);
+
   return (
     <div className="xxl:container mx-auto ">
       <section className="hero pt-20  bg-slate-200">
-        <div className=" relative flex flex-wrap md:flex-nowrap px-5 pb-20 sm:px-28 lg:justify-around xl:px-48 bg-yellow-200">
+        <div className=" relative flex flex-wrap md:flex-nowrap px-5 pb-20 sm:px-28 lg:justify-around xl:px-48 ">
           <div className="flex flex-wrap gap-5 justify-center self-center w-full text-center md:justify-start md:text-left leading-[40px]">
-            <p className="text-3xl md:text-4xl xl:text-5xl  font-medium text-purple-plum ">
+            <div className="text-3xl md:text-4xl xl:text-5xl  font-medium text-purple-plum ">
               Get aligned around your activity
               <span className="font-semibold text-dark-sea-green">
                 <Typewriter
@@ -16,11 +27,10 @@ const Hero = () => {
                     autoStart: true,
                     loop: true,
                     cursor: "&#10074",
-                    pauseFor: 1000,
                   }}
                 />
               </span>
-            </p>
+            </div>
             <p className="text-lg">
               turn your activity into a source of information instead of a list
               of frustration, make the timeframe & impact of your goals roadmap
@@ -45,22 +55,31 @@ const Hero = () => {
             <img src={heroImg} alt="" className="" />
           </div>
           <div className="w-full  md:absolute px-5 md:px-10 lg:px-48 left-0 -bottom-14 flex self-center justify-center">
-            <div className="bg-red-200 container w-full h-auto flex self-center justify-center gap-5 flex-wrap md:flex-nowrap">
-              <div className="bg-white flex flex-col text-center gap-7 w-full rounded-lg">
-                <h1 className="font-bold text-5xl text-slate-500">98%</h1>
-                <p>GTmetrix test performance</p>
-              </div>
-              <div className="bg-white flex flex-col text-center gap-7 w-full rounded-lg">
-                <h1 className="font-bold text-5xl text-slate-500">98%</h1>
-                <p>GTmetrix test performance</p>
-              </div>
-              <div className="bg-white flex flex-col text-center gap-7 w-full rounded-lg">
-                <h1 className="font-bold text-5xl text-slate-500">98%</h1>
-                <p>GTmetrix test performance</p>
-              </div>
-              <div className="bg-white flex flex-col text-center gap-7 w-full rounded-lg">
-                <h1 className="font-bold text-5xl text-slate-500">98%</h1>
-                <p>GTmetrix test performance</p>
+            <div className=" container w-full h-auto flex self-center justify-center gap-5 flex-wrap md:flex-nowrap">
+              <div className="stats w-full md:w-auto text-center md:text-left stats-vertical md:stats-horizontal shadow">
+                <div className="stat">
+                  <div className="stat-title">Web Performance</div>
+                  <div className="stat-value">
+                    <CountUp end={98} duration={1} />%
+                  </div>
+                  <div className="stat-desc text-violet-900">GTmetrik</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-title">New Users</div>
+                  <div className="stat-value">
+                    <CountUp end={randomUsers} duration={1} />
+                  </div>
+                  <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-title">New Registers</div>
+                  <div className="stat-value">
+                    <CountUp end={randomUsers} duration={1} />
+                  </div>
+                  <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
               </div>
             </div>
           </div>
