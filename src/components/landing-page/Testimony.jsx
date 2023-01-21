c44a22fa68ff754cd9366db15adc6c883383b418
Adanpradana <adanizkahariz@gmail.com>
2023-01-21 19:50:11 +0700

    add state

--- src/components/landing-page/Testimony.jsx
+++ src/components/landing-page/Testimony.jsx
@@ -1,6 +1,35 @@
 import { HiStar } from "react-icons/hi";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Testimony() {
+  const [name, setName] = useState("");
+  const [name2, setName2] = useState("");
+  const [name3, setName3] = useState("");
+  const [name4, setName4] = useState("");
+  const [comment, setComment] = useState("");
+  const [comment2, setComment2] = useState("");
+  const [comment3, setComment3] = useState("");
+  const [comment4, setComment4] = useState("");
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "https://api.quotable.io/random?maxLength=60",
+    })
+      .then((res) => console.log(res.data.author))
+      .then((res) => setName(res.data.author))
+      // .then((res) => setName2(res.data.author))
+      // .then((res) => setName3(res.data.author))
+      // .then((res) => setName4(res.data.author))
+      .then((res) => setComment(res.data.content));
+
+    // console.log(data);
+    // .then((res) => setComment2(res.data.author))
+    // .then((res) => setComment3(res.data.author));
+  }, []);
+  // console.log(data);
+
   return (
     <section className="w-full  bg-slate-200 p-5 lg:px-48">
       <div className="container m-auto w-full  bg-red-300  h-screen flex flex-wrap ">
@@ -34,13 +63,13 @@ export default function Testimony() {
             <p className="text-[0.9rem]">Rated 4,9/5 form over 485 review</p>
           </div>
         </div>
-        <div className="w-full bg-yellow-300 lg:flex-1 py-5 lg:py-0 lg:px-10 ">
+        <div className="w-full bg-yellow-300 lg:self-center lg:flex-1 py-5 lg:py-0 lg:px-10 ">
           <div className="container bg-slate-400  flex flex-col gap-3 lg:w-[500px] lg:p-5">
             <div className="bg-red-200  relative ">
               <div className="  flex self-center gap-5 bg-green-200 p-3">
                 <img src="https://i.pravatar.cc/100" alt="" />
                 <div className="flex flex-col gap-1 self-center">
-                  <h1 className="text-lg font-medium">Name</h1>
+                  <h1 className="text-lg font-medium">{name}</h1>
                   <p className="text-[0.9rem]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Nulla, voluptatum.
@@ -53,7 +82,7 @@ export default function Testimony() {
               <div className="  flex self-center gap-5 bg-green-200 p-3">
                 <img src="https://i.pravatar.cc/100" alt="" />
                 <div className="flex flex-col gap-1 self-center">
-                  <h1 className="text-lg font-medium">Name</h1>
+                  <h1 className="text-lg font-medium">{name}</h1>
                   <p className="text-[0.9rem]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Nulla, voluptatum.
@@ -66,7 +95,7 @@ export default function Testimony() {
               <div className="  flex self-center gap-5 bg-green-200 p-3">
                 <img src="https://i.pravatar.cc/100" alt="" />
                 <div className="flex flex-col gap-1 self-center">
-                  <h1 className="text-lg font-medium">Name</h1>
+                  <h1 className="text-lg font-medium">{name}</h1>
                   <p className="text-[0.9rem]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Nulla, voluptatum.
@@ -79,7 +108,7 @@ export default function Testimony() {
               <div className="  flex self-center gap-5 bg-green-200 p-3">
                 <img src="https://i.pravatar.cc/100" alt="" />
                 <div className="flex flex-col gap-1 self-center">
-                  <h1 className="text-lg font-medium">Name</h1>
+                  <h1 className="text-lg font-medium">{name}</h1>
                   <p className="text-[0.9rem]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Nulla, voluptatum.
