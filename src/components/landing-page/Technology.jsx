2d90fab2cbb8f1b4ba1546997aaae055a79af2f2
Adanpradana <adanizkahariz@gmail.com>
2023-01-23 21:24:41 +0700

    add slider image

--- src/components/landing-page/Technology.jsx
+++ src/components/landing-page/Technology.jsx
@@ -1,35 +1,39 @@
-import image from "../../data/images";
-
+import images from "../../data/images";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 export default function Technology() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    centerMode: true,
+  };
   return (
-    <section className="w-full  bg-slate-300  p-5 lg:px-20 xl:px-48">
-      <div className="container bg-red-200 w-full py-5">
+    <section className=" w-full bg-slate-300  p-5 lg:px-20 xl:px-0">
+      <div className="  w-full py-5">
         <div className="flex flex-col gap-5">
           <h1 className="text-center text-2xl font-semibold">
             Our Awesome Technology
           </h1>
-          <div className=" flex items-center min-h-screen justify-center">
-            {/* 1. */}
-            <div className="w-[200%] h-20 border-t border-b border-gray-600 overflow-hidden relative">
-              {/* 2. */}
-              <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate-animate  gap-5">
-                {/* 3 */}
-                {image.map((data) => {
-                  return (
-                    <div className="flex justify-center items-start w-[20rem] bg-blue-300">
-                      <img src={data} alt="" />
+          <div>
+            <Slider {...settings}>
+              {images.map((image) => {
+                return (
+                  <div className="container p-5">
+                    <div className="flex justify-center self-center bg-white shadow-md p-2 rounded-2xl w-auto ">
+                      <img src={image} alt="" className="h-[90px] " />
                     </div>
-                  );
-                })}
-                {image.map((data) => {
-                  return (
-                    <div className="flex justify-center items-start w-[20rem] bg-blue-300">
-                      <img src={data} alt="" />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
+                  </div>
+                );
+              })}
+            </Slider>
           </div>
         </div>
       </div>
