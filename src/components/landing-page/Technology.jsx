f75a5e7ba2e5934a3b152134a94b8a8c33fc6175
Adanpradana <adanizkahariz@gmail.com>
2023-01-25 06:13:15 +0700

    delete react-slick-carousel

--- src/components/landing-page/Technology.jsx
+++ src/components/landing-page/Technology.jsx
@@ -1,20 +1,6 @@
 import images from "../../data/images";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 export default function Technology() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 3000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrows: false,
-    autoplaySpeed: 0,
-    cssEase: "linear",
-    centerMode: true,
-  };
   return (
     <section className=" w-full bg-slate-300  p-5 lg:px-20 xl:px-0">
       <div className="  w-full py-5">
